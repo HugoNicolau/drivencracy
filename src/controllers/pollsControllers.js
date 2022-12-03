ea313c49f052db7c0b9238c670e8482b07a40c9d
HugoNicolau <nicolau.hugogiles@gmail.com>
2022-12-03 08:57:49 -0300

    Fix: returning 422 if poll doesnt have title

--- src/controllers/pollsControllers.js
+++ src/controllers/pollsControllers.js
@@ -3,17 +3,14 @@ import { pollsCollection } from "../database/db.js";
 
 export async function postPoll(req, res){
     const { title, expireAt } = req.body
-    if(title.length < 1){
-        return res.send("Título não pode ser vazio").status(422);
+    if(!title){
+        return res.status(422).send("Título não pode ser vazio");
     }
-    // if(!expireAt){
-    //     expireAt = dayjs().format(`YYYY/MM/DD`).padEnd(30, 'day');
-    // }
-
+  
     try{
         const newPoll = {title:title, expireAt:(!expireAt ? dayjs().add(30, 'day').format(`YYYY/MM/DD HH:mm`) : expireAt )}
         await pollsCollection.insertOne(newPoll)
-        return res.send(newPoll).status(201);
+        return res.status(201).send(newPoll);
     } catch(err){
         console.log(err);
         return res.sendStatus(500);
