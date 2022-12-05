9e8244551d14f6cf98b1f99f835a8a0a3e7efeb5
HugoNicolau <nicolau.hugogiles@gmail.com>
2022-12-05 08:03:17 -0300

    Get choices refact and validating objectId

--- src/controllers/choicesControllers.js
+++ src/controllers/choicesControllers.js
@@ -1,36 +1,29 @@
-import { ObjectId } from "mongodb";
-import { choicesCollection, pollsCollection } from "../database/db.js";
+import { choicesCollection } from "../database/db.js";
 
+export async function postChoice(req, res) {
+  const choice = res.locals.choice;
 
-
-
-export async function postChoice(req, res){
-    const choice = res.locals.choice;
-
-    try{
-        await choicesCollection.insertOne(choice)
-        return res.status(201).send(choice)
-    }catch(err){
-        console.log(err);
-        return res.sendStatus(500);
-    }
+  try {
+    await choicesCollection.insertOne(choice);
+    return res.status(201).send(choice);
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(500);
+  }
 }
 
+export async function getChoices(req, res) {
+  const id = res.locals.id;
 
-export async function getChoices(req, res){
-    const id = res.locals.id;
-
-
-   
-    try{
-        const pollChoices = await choicesCollection.find({pollId:id}).toArray();
-        if(pollChoices.length<1){
-            return res.sendStatus(404);
-        }
-
-        return res.status(200).send(pollChoices);
-    }   catch(err){
-        console.log(err);
-        return res.sendStatus(500);
+  try {
+    const pollChoices = await choicesCollection.find({ pollId: id }).toArray();
+    if (pollChoices.length === 0 || !pollChoices) {
+      return res.sendStatus(404);
     }
+
+    return res.status(200).send(pollChoices);
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(500);
+  }
 }
